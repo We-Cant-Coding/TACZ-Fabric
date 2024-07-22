2d96ca299c2f40676e4e18a515c2671520f17187
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2024-07-22 15:12:54 +0900

    Bug fixed & Switching from mixing to possible events

---
+++ src/main/java/com/tacz/guns/GunMonMixinPlugin.java
@@ -0,0 +1,50 @@
+package com.tacz.guns;
+
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class GunMonMixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        var fabricLoader = FabricLoader.getInstance();
+        return switch (mixinClassName) {
+            case "com.tacz.guns.mixin.common.RaycastContextMixin" ->
+                    !fabricLoader.isModLoaded("porting_lib_base");
+            default -> true;
+        };
+    }
+
+    @Override
+    public void onLoad(String mixinPackage) {
+        MixinExtrasBootstrap.init();
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
